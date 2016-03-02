82feaca5e258fc80a7bf1a93d4dbe56874abf7ce
Matthew Martens <matthew.s.martens@gmail.com>
2016-03-01 19:44:00 -0500

    Remove coding and shebang lines

--- fizzbotz/__init__.py
+++ fizzbotz/__init__.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 from .responses import Imgur, Insult, Joke, Roll, Square, TwitchChat
 from .exceptions import FizzbotzException, EmptyStringError, StringLengthError
 from .util import Buffer, get_markup
